be68854d06292ff48b46f59568c27617e4f0a4d4
Paul Williams <p.b.williams@outlook.com>
2022-08-16 16:25:25 +0100

    Added

---
+++ PW.Common/IO/FileSystemObjects/Paths/FilePath.Read.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW.IO.FileSystemObjects;
+
+public partial class FilePath
+{
+
+  public string ReadAllText() => File.ReadAllText(Path);
+
+  public string[] ReadAllLines => File.ReadAllLines(Path);
+
+  public byte[] ReadAllBytes => File.ReadAllBytes(Path);
+
+
+}
